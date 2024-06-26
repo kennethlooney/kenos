0f78b0bfc415aee5053425a731143695b72ffc1f
Kenneth Looney <kenneth.looney@outlook.com>
2024-06-25 23:25:35 -0700

    Fix: Changed a library name

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-qemu-system-x86_64 -name KENOS -bios bios64.bin -net none -hda fat:rw:image
+# From image
+#qemu-system-x86_64 -drive format=raw,unit=0,file=kenos.img -name KENOS -bios bios64.bin -m 256M -vga std -machine q35 -net none
+
+# From directory
+qemu-system-x86_64 -name KENOS -bios bios64.bin -m 256M -vga std -machine q35 -net none -hda fat:rw:image
