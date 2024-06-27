10877c9218307aea7d1626c407ac2f62d18bb653
Kenneth Looney <kenneth.looney@outlook.com>
2024-06-26 20:34:53 -0700

    Cleaned up project

--- projects/tools/createdisk/main.c
+++ projects/tools/createdisk/main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "diskformats/mbr.h"
+ 
 
 char* image_name = "disk.img";
-uint64_t lba_size = 512;
-uint64_t esp_size = 1024*1024*33;   // 33mb
-uint64_t data_size = 1024*1024*1;   // 1mb
-uint64_t image_size = 0;
-uint64_t esp_lbas, data_lbas, image_size_lbas;
+
 int main(int argc, char** argv)
 {
     FILE* image = fopen(image_name, "wb+");
@@ -16,15 +12,9 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     } 
 
-    // Set sizes
-    image_size = esp_size + data_size + (1024*1024);    // Add some padding
-    image_size_lbas = bytes_to_lbas(image_size, lba_size);
+    
 
-    if(!write_mbr(image, image_size_lbas))
-    {
-        fprintf(stderr, "Error: could not write protective MBR to image '%s'\n", image_name);
-        return EXIT_FAILURE;
-    }
+ 
     
 
 
